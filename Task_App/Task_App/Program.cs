d68f5b43d12de2d42caa152b79df6b2a59e89b1f
Kostyantin <kosonischenko@gmail.com>
2018-01-08 14:32:20 +0300

    rename conrole.writeline

--- Task_App/Task_App/Program.cs
+++ Task_App/Task_App/Program.cs
@@ -16,7 +16,7 @@ namespace Task_App
 
         public static void Eight()
         {
-            Console.WriteLine("task number eight: ");
+            Console.WriteLine("task number 8: ");
             for (int i = 1; i < 21; i++)
             {
                 Console.Write(i + " ");
@@ -27,7 +27,7 @@ namespace Task_App
         public static void Ten()
         {
             Console.WriteLine();
-            Console.WriteLine("task number ten: ");
+            Console.WriteLine("task number 10: ");
             for (int i = 100; i > 0; i-=4)
             {
                 Console.Write(i + " ");
