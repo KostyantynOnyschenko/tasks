7c6bfe289b99a88b2641a2a397f7d67059617810
Kostyantin <kosonischenko@gmail.com>
2018-03-09 20:57:51 +0300

    project 2 task 3

--- Task_App/TaskTwo_App/Program.cs
+++ Task_App/TaskTwo_App/Program.cs
@@ -11,6 +11,7 @@ namespace TaskTwo_App
         static void Main(string[] args)
         {
             Two();
+            Three();
         }
 
         /// <summary>
@@ -18,14 +19,44 @@ namespace TaskTwo_App
         /// </summary>
         public static void Two()
         {
-            Console.WriteLine("task number 2: ");
+            Console.WriteLine("-------------------------task number 2: ");
             Console.WriteLine("write meters: ");
             double n = Int32.Parse(Console.ReadLine());
             Console.WriteLine(n + " meters");
             double km;
             km = n / 1000;
             Console.Write(km + " km");
-            Console.ReadKey();         
+            Console.ReadKey();
+            Console.ReadLine();
         }
+
+        /// <summary>
+        /// <para>Составить программу, которая требует ввести два числа.</para>
+        /// <para> Если первое число больше второго, то программа печатает слово больше. </para>
+        /// <para>Если первое число меньше второго, то программа печатает слово меньше. </para>
+        /// <para>А если числа равны, программа напечатает сообщение Эти числа равны.</para>
+        /// </summary>
+        public static void Three()
+        {
+            Console.WriteLine("-------------------------task number 3: ");
+            Console.WriteLine("write digital number 1: ");
+            int n_1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("write digital number 2: ");
+            int n_2 = Int32.Parse(Console.ReadLine());
+            if (n_1 > n_2)
+            {
+                Console.WriteLine("first digital larger");
+            }
+            else if (n_1 < n_2)
+            {
+                Console.WriteLine("second digital larger");
+            }
+            else
+            {
+                Console.WriteLine("this diital equal");
+            }
+            Console.ReadKey();
+        }
+
     }
 }
