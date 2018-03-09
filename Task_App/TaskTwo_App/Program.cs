11ae20edcbccf69bbd5142af04598fbbb74c253b
Kostyantin <kosonischenko@gmail.com>
2018-03-09 20:44:05 +0300

    project 2 task 2

--- Task_App/TaskTwo_App/Program.cs
+++ Task_App/TaskTwo_App/Program.cs
@@ -10,8 +10,22 @@ namespace TaskTwo_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("new project");
-            Console.ReadLine();
+            Two();
+        }
+
+        /// <summary>
+        /// <para>Программа должна переводить число, введенное с клавиатуры с метра в километры.</para>
+        /// </summary>
+        public static void Two()
+        {
+            Console.WriteLine("task number 2: ");
+            Console.WriteLine("write meters: ");
+            double n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(n + " meters");
+            double km;
+            km = n / 1000;
+            Console.Write(km + " km");
+            Console.ReadKey();         
         }
     }
 }
