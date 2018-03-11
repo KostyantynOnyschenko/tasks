4be56ad1e7afb48604ce2b1a11e4a586af029473
Kostyantin <kosonischenko@gmail.com>
2018-03-11 12:32:08 +0300

    task one part 2

--- Task_App/TaskTwoPart1_App/Program.cs
+++ Task_App/TaskTwoPart1_App/Program.cs
@@ -10,8 +10,25 @@ namespace TaskTwoPart1_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("part 2");
-            Console.ReadLine();
+            One();
         }
+
+        /// <summary>
+        /// <para>Вычислить значение выражения по формуле (1-tg x)^(ctg x)+ cos(x-y).</para>
+        /// </summary>
+        public static void One()
+        {
+            Console.WriteLine("-------------------------task number 1: ");
+            Console.WriteLine("введите x: ");
+            double x = double.Parse(Console.ReadLine());
+            Console.WriteLine("введите y: ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("(1-tg x)^(ctg x)+ cos(x-y) = ");
+            double ctg = 1 / Math.Tan(x);
+            x = ((1 - Math.Tan(x) * Math.Pow(ctg, x)+ (Math.Cos(x) - Math.Cos(y))));
+            Console.Write(x);
+            Console.ReadKey();
+        }
+
     }
 }
