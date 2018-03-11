010a819d2d9765b84876874972847c925a8ccc11
Kostyantin <kosonischenko@gmail.com>
2018-03-11 14:47:10 +0300

    begin task 6

--- Task_App/TaskTwoPart1_App/Program.cs
+++ Task_App/TaskTwoPart1_App/Program.cs
@@ -28,7 +28,17 @@ namespace TaskTwoPart1_App
             x = ((1 - Math.Tan(x) * Math.Pow(ctg, x)+ (Math.Cos(x) - Math.Cos(y))));
             Console.Write(x);
             Console.ReadKey();
+            Console.ReadLine();
         }
 
+        /// <summary>
+        /// <para>Программа проверяет, является ли введенное с клавиатуры число — целым числом.</para>
+        /// </summary>
+        public static void Six()
+        {
+            Console.WriteLine("-------------------------task number 6: ");
+            Console.WriteLine("введите число: ");
+
+        }
     }
 }
