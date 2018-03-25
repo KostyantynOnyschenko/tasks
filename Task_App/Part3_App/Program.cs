f932fa2c34b236541058c22508ffa6d92ffaf477
Kostyantin <kosonischenko@gmail.com>
2018-03-25 19:14:57 +0300

    part 3 task 1

---
+++ Task_App/Part3_App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+
+namespace Part3_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            One();
+        }
+
+        /// <summary>
+        /// <para>Вычислить факториал введенного числа.</para>
+        /// </summary>
+        private static void One()
+        {
+            Console.WriteLine("-------------------------task number 1: ");
+            Console.WriteLine("введите число: ");
+            int dig = Int32.Parse(Console.ReadLine());           
+            Console.WriteLine("Факториал введенного числа равен: " + SpecialFunctions.Factorial(dig));
+            Console.ReadKey();
+
+        }
+
+    }
+}
