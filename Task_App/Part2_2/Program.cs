e10829ca1c98ad7a57e0c2f710957c76ab7acb3f
Kostyantin <kosonischenko@gmail.com>
2018-03-26 20:56:52 +0300

    task 2

--- Task_App/Part2_2/Program.cs
+++ Task_App/Part2_2/Program.cs
@@ -10,6 +10,29 @@ namespace Part2_2
     {
         static void Main(string[] args)
         {
+            Two();
         }
+
+        /// <summary>
+        /// <para>В массиве найти минимальный и максимальный элементы, поменять их местами.</para>
+        /// </summary>
+        private static void Two()
+        {
+            Console.WriteLine("-------------------------task number 2: ");
+            int[] arr = {1, 5, 6, 4, 9, 21, 12, 9, 945, 47, 46, 79, 23};
+            int min, max;
+            min = max = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+                if (arr[i] > max) max = arr[i];
+                if (arr[i] < min) min = arr[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine("max: " + max + " min: " + min);
+            Console.ReadLine();
+        }
+
+
     }
 }
